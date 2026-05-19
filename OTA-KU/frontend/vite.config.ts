aea93026b9e510557814b69f4b824e88755b59bd
indahtangdililing <indahtangdililing@gmail.com>
2026-05-19 21:08:56 +0700

    feat: nginx subdomain

--- OTA-KU/frontend/vite.config.ts
+++ OTA-KU/frontend/vite.config.ts
@@ -7,6 +7,7 @@ import istanbul from "vite-plugin-istanbul";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ota",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
